a8015db2728ca3e09765c059e3c3f6cb30ff2a9c
Maksym Shapovalov <maks0681912507@gmail.com>
2023-06-14 22:31:24 +0300

    Configure Client CI/CD

--- packages/client/.eslintrc.cjs
+++ packages/client/.eslintrc.cjs
@@ -1,7 +1,9 @@
 module.exports = {
   extends: [
     "../../.eslintrc",
-    "plugin:react/recommended"
+    "plugin:react/recommended",
+    "react-app",
+    "react-app/jest"
   ],
   env: {
     browser: true
