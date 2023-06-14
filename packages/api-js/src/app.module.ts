8a4e4c56b732ed5354f1bb82e0db75f5864fe238
Maksym Shapovalov <maks0681912507@gmail.com>
2023-06-14 22:31:50 +0300

    Configure API-JS CI/CD

--- packages/api-js/src/app.module.ts
+++ packages/api-js/src/app.module.ts
@@ -1,8 +1,7 @@
 import { Module } from '@nestjs/common';
+import { AppController } from './app.controller';
 
 @Module({
-    imports: [],
-    controllers: [],
-    providers: [],
+    controllers: [AppController],
 })
 export class AppModule {}
